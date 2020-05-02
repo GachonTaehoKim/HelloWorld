bd1cd7d93c82638ef31f036d41fbe7b951e48c45
kthterran <31417849+kthterran@users.noreply.github.com>
2020-05-02 16:56:29 +0900

    필요없는내용삭제

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -5,14 +5,17 @@
 	1.Hello, World 출력
 	2.Github 사용해보기
 	3.새로운 분기점을 만들어서 push 해보기
+
+	4. 수정하기
 */
 
 #include <iostream>
-#include <string>
-
+//#include <string>
+using std::cout;
 int main() {
-	//cout << "Hello, World!";
-	std::string str = "Hello, World!";
-	std::cout << str;
+
+	cout << "Hello, World!";
+	//std::string str = "Hello, World!";
+	//std::cout << str;
 	return 0;
 }
