158eb5801a3aacd53869b5c8325ac3c158d75777
kthterran <31417849+kthterran@users.noreply.github.com>
2020-05-02 16:49:56 +0900

    주석추가, string 타입으로 출력

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -4,11 +4,15 @@
 제작 내용:	
 	1.Hello, World 출력
 	2.Github 사용해보기
+	3.새로운 분기점을 만들어서 push 해보기
 */
 
 #include <iostream>
+#include <string>
 
 int main() {
-	std::cout << "Hello, World!";
+	//cout << "Hello, World!";
+	std::string str = "Hello, World!";
+	std::cout << str;
 	return 0;
 }
